03f6dcae383cbede5d7546e595684342a94f258c
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-10-17 17:01:08 +0300

    add Exampl 009, 010, 011

---
+++ Exampl010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿// Имеется одномерный массив array из n элементов, требуется найти элемент массива, равный find
+
+int[] array = {1, 2, 3, 4, 5, 16, 7, 8};
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find) 
+    {
+        Console.WriteLine(index); 
+    }
+    //index = index + 1;
+    index++;
+}
